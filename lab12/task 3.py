fb73bdafdd75308b65456f1e2c684b0621549e93
Misha_si <128136138+GLISTMISHA2@users.noreply.github.com>
2024-12-09 21:41:47 +0700

    Update task 3.py

--- lab12/task 3.py
+++ lab12/task 3.py
@@ -1,19 +1,23 @@
 import PySimpleGUI as sg
 import random as r
 
-# Инициализация графического интерфейса
+# Настройки темы и макета для калькулятора бактерий
+sg.theme("Reddit")
 c_image = [[sg.Image("bio.png")]]
 c_input = [
-    [sg.Text("Введите количество секунд:", font="Arial 20"), sg.Input(font="Arial 20", size=(5, 0), key="count")],
-    [sg.Text("На сколько делиться (k):", font="Arial 20"), sg.Input(font="Arial 20", size=(5, 0), key="kol")],
+    [sg.Text("Введите количество бактерий:", font="Arial 20"), sg.Input(font="Arial 20", size=(5, 0), key="micro")],
+    [sg.Text("Количество секунд:", font="Arial 20"), sg.Input(font="Arial 20", size=(5, 0), key="count")],
+    [sg.Text("На сколько делиться:", font="Arial 20"), sg.Input(font="Arial 20", size=(5, 0), key="kol")],
     [sg.Text("Результат:", font="Arial 20"), sg.Input(font="Arial 20", readonly=True, size=(5, 0), key="res")],
     [sg.Button("Рассчитать", font="Arial 20")]
 ]
 
+# Объединение макетов
 layout = [
     [sg.Column(c_image), sg.VSeperator(), sg.Column(c_input, justification='right')]
 ]
 
+# Создание окна
 window = sg.Window("Калькулятор бактерий", layout)
 
 while True:
@@ -24,17 +28,19 @@ while True:
 
     if event == "Рассчитать":
         try:
-            count = int(value["count"])  # Количество секунд для расчета
-            kol = int(value["kol"])      # На сколько делиться (k)
+            micro = int(value["micro"])  # изначальное количество бактерий
+            count = int(value["count"])  # количество секунд для расчета
+            kol = int(value["kol"])      # коэффициент деления
+            res = micro                  # начальное количество бактерий
 
-            micro = 1  # Начальное количество бактерий
-
+            # Расчет количества бактерий на N-й секунде
             for _ in range(count):
-                b = r.randint(0, 4)  # Рандомное число b в диапазоне от 0 до 4
-                micro = kol * micro + b
+                b = r.randint(0, 4)
+                res = kol * res + b
 
-            window["res"].update(micro)
+            # Обновление результата в интерфейсе
+            window["res"].update(res)
         except ValueError:
-            sg.popup_error("Пожалуйста, введите корректные числовые значения.")
+            window["res"].update("Ошибка ввода")
 
 window.close()
